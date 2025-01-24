d2c259699091eb8ae8ab1e59ea81810eb625ae24
MonicahMackena¨ <mackenamuthui@gmail.com>
2025-01-24 17:04:54 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState,useEffect } from "react";
+
+
+function App() {
+  const [dogsImage,setDogsImage ]= useState([])
+  const [Loading, setLoading] = useState(true)
+
+  useEffect(()=>{ 
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+    setDogsImage(data.message);
+    setLoading(false)
+  })
+    .catch(error=> 
+    {console.error(`The server returned an error: ${error}`);
+     setLoading(false)
+  })
+},[])
+return (
+  <div>
+    {Loading ? <p>Loading........</p> : (<img src={dogsImage} alt="A Random Dog"/>)}
+  </div>
+)
+}
+export default App;
